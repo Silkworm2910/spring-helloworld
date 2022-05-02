2937dd344cba6ed4ae9538c4d2d994c39b882e22
Anastasia Akkerman <silkworm2910@gmail.com>
2022-05-03 02:58:07 +0300

    done

---
+++ src/main/java/Cat.java
@@ -0,0 +1,35 @@
+import java.util.Objects;
+
+public class Cat {
+    private String name;
+    private byte age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return age == cat.age && name.equals(cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
